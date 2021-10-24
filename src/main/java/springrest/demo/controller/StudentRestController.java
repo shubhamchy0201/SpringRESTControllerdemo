ef303e803e675174ed2ab1e29463255d7afbef34
Shubham kumar choudhary <shubhamchoudhary7114@gmail.com>
2021-10-24 21:00:19 +0530

    Spring REST Rest demo code added

---
+++ src/main/java/springrest/demo/controller/StudentRestController.java
@@ -0,0 +1,108 @@
+package springrest.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import springrest.demo.entity.Student;
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+	/*
+	// define endpoint for "/students" - return list of students
+	
+	@GetMapping("/students")
+	public List<Student> getStudents() {
+
+		List<Student> theStudents = new ArrayList<>();
+		
+		theStudents.add(new Student("shubham", "kumar"));
+		theStudents.add(new Student("amit", "karn"));
+		theStudents.add(new Student("sumit", "Smith"));
+			
+		return theStudents;
+	}
+
+	 */
+
+
+	private List<Student> theStudents;
+
+	// define @PostConstruct to load the student data ... only once!
+
+	@PostConstruct
+	public void loadData() {
+
+		theStudents = new ArrayList<>();
+
+		theStudents.add(new Student("shubham", "kumar"));
+		theStudents.add(new Student("sumit", "patel"));
+		theStudents.add(new Student("karn", "Smith"));
+	}
+
+
+
+	// define endpoint for "/students" - return list of students
+
+	@GetMapping("/students")
+	public List<Student> getStudents() {
+
+		return theStudents;
+	}
+
+	// define endpoint for "/students/{studentId}" - return student at index
+
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+
+
+		// just index into the list ... keep it simple for now
+
+		// check the studentId against list size
+
+		if ( (studentId >= theStudents.size()) || (studentId < 0) ) {
+			throw new StudentNotFoundException("Student id not found - " + studentId);
+		}
+
+		return theStudents.get(studentId);
+
+	}
+/*
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+
+		StudentErrorResponse error = new StudentErrorResponse();
+
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+	}
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
+
+		StudentErrorResponse error = new StudentErrorResponse();
+
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+	}
+
+ */
+}
+
+
+
+
+
+
+
+
+
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<body>
+<h2>Spring Rest Controller Demo</h2>
+<hr>
+<a href="test/hello">Hello </a>
+<br>
+<a href="api/students">Get All students </a>
+</body>
+</html>
